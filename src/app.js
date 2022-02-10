5cbdcbf5b510f70f7bc46dbf891059d96e34b873
john <jenweani@gmail.com>
2022-02-10 15:40:20 +0100

    sec init commit

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require('./utils/forecast')
 const getGeoCode = require('./utils/geocode')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -79,6 +80,6 @@ app.get('*', (req, res) => {
         message: 'Wow such empty! NO such page' })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`)
 })
